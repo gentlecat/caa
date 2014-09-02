ee30a69407a5eba8f8d41144f7cfe45c4c23fd2b
Roman Tsukanov <roman@tsukanov.me>
2014-09-02 23:58:19 +1000

    Created initial prototype.

---
+++ caa.go
@@ -0,0 +1,65 @@
+// go-caa is a client library for Cover Art Archive.
+/*
+Cover Art Archive (CAA) is a project whose goal is to make cover art images
+available to everyone on the Internet in an organized and convinient
+format. More informantion is available at https://coverartarchive.org/.
+
+There are two supported MusicBrainz ID (MBID) types for retrieval from CAA:
+ * release
+ * release group
+
+*/
+package caa
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Image struct {
+	ID             string   `json:"id"`
+	Image          string   `json:"image"`    // Full URL to the original image
+	Approved       bool     `json:"approved"` // Whether the image was approved by the MusicBrainz edit system
+	Comment        string   `json:"comment"`  // A free text comment
+	Edit           int      `json:"edit"`     // Edit ID on MusicBrainz
+	LargeThumbnail string   `json:"thumbnails.large"`
+	SmallThumbnail string   `json:"thumbnails.small"`
+	Types          []string `json:"types"`
+	IsFront        bool     `json:"front"`
+	IsBack         bool     `json:"back"`
+}
+
+var (
+	Scheme = "http" // http or https
+	Host   = "coverartarchive.org"
+)
+
+func getServerURL() string { return Scheme + "://" + Host }
+
+// GetRelease returns information about cover art image for a release group
+// with a specified MusicBrainz ID.
+func GetRelease(mbid string) ([]Image, error) {
+	resp, err := http.Get(getServerURL() + "/release/" + mbid)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	type jason struct {
+		Images []Image
+	}
+
+	respParsed := jason{}
+	err = json.Unmarshal(body, &respParsed)
+	if err != nil {
+		return nil, err
+	}
+
+	return respParsed.Images, nil
+}
